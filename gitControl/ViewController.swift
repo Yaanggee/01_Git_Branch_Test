5539284e43c514eb07c4ac3b0c596e6999fcb92d
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2017-09-12 10:35:45 +0900

    add button and add print second commit

--- gitControl/ViewController.swift
+++ gitControl/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("1st Commit")
+        print("second Commit")
     }
 
     override func didReceiveMemoryWarning() {
